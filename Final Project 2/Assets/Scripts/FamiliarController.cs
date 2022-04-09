b7bfe798b1ac18c662355459c529f5da7a1c6cf5
vHepp <vuhepola1227@gmail.com>
2022-04-08 22:32:14 -0400

    Working toggle for familiar gameobjects. Looking at doorframe while switching to block_familiar will activate blocking

--- Final Project 2/Assets/Scripts/FamiliarController.cs
+++ Final Project 2/Assets/Scripts/FamiliarController.cs
@@ -9,7 +9,7 @@ public class FamiliarController : MonoBehaviour
 	public GameObject[] familiarList;
 	public int activeFamiliar;
 
-/* 	// Movement Stuff
+	// Movement Stuff
 	public GameObject player;
 	public float distanceFromPlayer;
 	public float distancePerSecond = 1f;
@@ -19,7 +19,15 @@ public class FamiliarController : MonoBehaviour
 	public float nextTimeout;
 	public float mTimeoutDuration = .2f;
 	public float iTimeoutDuration = .2f;
-	public int moveState = 0; */
+	public int moveState = 0;
+
+	//Block Stuff
+	public GameObject[] blockLocations;
+	public int locationBlocked;
+	public Camera fpsCam; // player's camera
+	public LayerMask TargetLayer; // Layer of BlockLocation
+	public RaycastHit hit;
+	public bool isBlocking = false;
 
 
 	// Start is called before the first frame update
@@ -32,26 +40,112 @@ public class FamiliarController : MonoBehaviour
 		familiarList[0].SetActive(true);
 		familiarList[1].SetActive(false);
 		activeFamiliar = 0;
+		
+		// Block stuff
+		blockLocations = GameObject.FindGameObjectsWithTag("BlockLocation");
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		//current += Time.deltaTime;
+		current += Time.deltaTime;
 
 		// "Command" = "v"
 		if(Input.GetButtonDown("Command")){
 			if(activeFamiliar == 0){
+				//switch to blocking
 				familiarList[1].SetActive(true);
 				familiarList[0].SetActive(false);
 				activeFamiliar = 1;
+				distanceFromPlayer = 1f;
+
+				isBlocking = StartBlock(); 
 			}
 			else
 			{
+				//switch to following
 				familiarList[0].SetActive(true);
 				familiarList[1].SetActive(false);
 				activeFamiliar = 0;
+				distanceFromPlayer = 2.5f;
 			}
 		}
+
+		if(activeFamiliar == 0){
+			// Follow
+			distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+			if (distanceFromPlayer > outerRadius)
+			{
+				if (moveState == 0 && current >= nextTimeout)
+				{
+					transform.position = Vector3.MoveTowards(transform.position, player.transform.position, distancePerSecond * Time.deltaTime);
+				}
+				else if (moveState == 1 && current >= nextTimeout)
+				{
+					moveState = 0; // fix state error
+				}
+				else if (moveState == 2)
+				{
+					nextTimeout = current + iTimeoutDuration;
+					moveState = 0;
+				}
+			}
+			else if (distanceFromPlayer <= outerRadius && distanceFromPlayer >= innerRadius)
+			{
+				if (moveState == 0)
+				{
+					nextTimeout = current + mTimeoutDuration;
+					moveState = 1;
+				}
+				if (moveState == 1)
+				{
+					if (current >= nextTimeout)
+					{
+						moveState = 2;
+					}
+					else
+					{
+						transform.position = Vector3.MoveTowards(transform.position, player.transform.position, distancePerSecond * Time.deltaTime);
+					}
+				}
+			}
+			else if (distanceFromPlayer < innerRadius)
+			{
+				moveState = 2;
+			}
+
+		}
+		else if(activeFamiliar == 1 && isBlocking){
+			Transform t = hit.transform;
+			transform.position = Vector3.MoveTowards(transform.position, t.position, distancePerSecond * Time.deltaTime);
+			if (transform.position.x == t.position.x && Mathf.Abs(transform.position.y - t.position.y) < .1 && transform.position.z == t.position.z)
+			{
+				transform.position = t.position;
+				transform.rotation = t.rotation;
+				isBlocking = false;
+			}
+		}	
 	}
+
+	bool StartBlock()
+	{
+		// raycast for placeable 
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 10f, TargetLayer))
+		{
+			Debug.Log("Hit something!");
+			if (hit.collider.isTrigger)
+			{
+				Debug.Log("Hit the trigger!");
+				return true;
+			}
+			else
+			{
+				Debug.Log("Not a trigger!");
+			}
+		}
+		return false;
+
+	}
+
 }
